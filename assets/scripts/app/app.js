cc3bd6ac69c5036ef0dd5b818048af2d96c9e877
Artur Gulowaty <a.gulowaty@gmail.com>
2018-03-01 14:05:35 +0000

    close button implemented

--- assets/scripts/app/app.js
+++ assets/scripts/app/app.js
@@ -1,5 +1,6 @@
 (function() {
 	function toggleExpander(e) {
+		e.preventDefault();
 		var contentElCls = e.currentTarget.nextElementSibling.classList;
 		var openedClassName = 'expander__item__content--open';
 		var clickedArrowImg = e.currentTarget.querySelector('img.expander__item__title__arrow');
@@ -16,6 +17,14 @@
 	document.addEventListener('DOMContentLoaded', function () {
 		forEachElement(document, '.expander__item__title', function(e) {
 			e.onclick = toggleExpander;
+		});
+
+		forEachElement(document, '.expander__item__button--close', function(e) {
+			e.onclick = function(evt) {
+				var parentTitle = evt.currentTarget.closest('.expander__item').querySelector('.expander__item__title');
+				parentTitle.click();
+				parentTitle.scrollIntoView();
+			}
 		});
   	})
 })();
