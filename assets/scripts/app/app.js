e3687227735c8fe419425d1c9ac2145687db0f33
Artur Gulowaty <a.gulowaty@gmail.com>
2018-03-06 12:36:09 +0000

    added polyfill for .closest()

--- assets/scripts/app/app.js
+++ assets/scripts/app/app.js
@@ -1,6 +1,22 @@
 (function() {
+	// polyfill for closest function
+	if (!Element.prototype.matches) {
+		Element.prototype.matches = Element.prototype.msMatchesSelector || 	Element.prototype.webkitMatchesSelector;
+	}
+	if (!Element.prototype.closest) {
+		Element.prototype.closest = function(s) {
+			var el = this;
+			if (!document.documentElement.contains(el)) return null;
+			do {
+				if (el.matches(s)) return el;
+				el = el.parentElement || el.parentNode;
+			} while (el !== null && el.nodeType === 1);
+			return null;
+		};
+	}
+
 	function toggleExpander(e) {
-		e.preventDefault();
+			e.preventDefault();
 		var contentElCls = e.currentTarget.nextElementSibling.classList;
 		var openedClassName = 'expander__item__content--open';
 		var clickedArrowImg = e.currentTarget.querySelector('img.expander__item__title__arrow');
